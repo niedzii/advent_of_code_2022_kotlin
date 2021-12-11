36a8cdd1f548161119f2d00b95c264eafb4a96cc
niedzii <niedzwiad14@gmail.com>
2021-12-11 13:20:01 +0100

    day 9 part 2

--- src/day9/Day9.kt
+++ src/day9/Day9.kt
@@ -1,57 +1,103 @@
 package day9
 
 import java.io.File
-import java.lang.Character.getNumericValue
+import java.util.LinkedList
+import java.util.Queue
 
 fun main() {
-    val input = File("src/day9", "day9_input.txt").readLines()
+    val input = File("src/day9", "day9_input.txt").readLines().map { it.toCharArray().map(Character::getNumericValue) }
     println("Stage 1 answer is ${stageOne(input)}") // 572
-    println("Stage 2 answer is ${stageTwo(input)}")
+    println("Stage 2 answer is ${stageTwo(input)}") // 847044
 }
 
-fun stageOne(input: List<String>): Int {
-    val height = input.size
-    val width = input[0].length
+fun stageOne(input: List<List<Int>>): Int {
+    return findLowestPointsCords(input).map { input[it.first][it.second] }.sumOf { it + 1 }
+}
 
-    var result = 0
+fun stageTwo(input: List<List<Int>>): Int {
+    return findLowestPointsCords(input).map {
+        calculateBasicSizeFromLowestPoint(input, it.first, it.second)
+    }.sortedDescending().subList(0, 3).reduceRight { left, right -> left * right }
+}
 
-    input.forEachIndexed { index, it ->
+fun calculateBasicSizeFromLowestPoint(grid: List<List<Int>>, row: Int, col: Int): Int {
+
+    val vis = MutableList(grid.size) { Array(grid[0].size) { false } }
+    val rowCords = intArrayOf(-1, 0, 1, 0)
+    val columnCords = intArrayOf(0, 1, 0, -1)
+
+    val queue: Queue<Pair<Int, Int>> = LinkedList()
+    var size = 0
+
+    queue.add(Pair(row, col))
+    vis[row][col] = true
+
+    while (!queue.isEmpty()) {
+        val cell: Pair<Int, Int> = queue.peek()
+        val x: Int = cell.first
+        val y: Int = cell.second
+        size++
+        queue.remove()
+
+        // Go to the adjacent cells
+        for (i in 0..3) {
+            val adjacentX: Int = x + rowCords[i]
+            val adjacentY: Int = y + columnCords[i]
+
+            val value = try {
+                grid[adjacentX][adjacentY]
+            } catch (e: IndexOutOfBoundsException) {
+                // if its out of grid it can be treated as peek height
+                9
+            }
+
+            if (isValid(vis, adjacentX, adjacentY, value)) {
+                queue.add(Pair(adjacentX, adjacentY))
+                vis[adjacentX][adjacentY] = true
+            }
+        }
+    }
+    return size
+}
+
+fun isValid(vis: List<Array<Boolean>>, row: Int, col: Int, value: Int): Boolean {
+    // If cell lies out of bounds or is peek height
+    if (row < 0 || col < 0 || row >= vis.size || col >= vis[0].size || value == 9) return false
+    return !vis[row][col]
+}
+
+fun findLowestPointsCords(grid: List<List<Int>>): List<Pair<Int, Int>> {
+    val height = grid.size
+    val width = grid[0].size
+    val result = mutableListOf<Pair<Int, Int>>()
+    grid.forEachIndexed { index, it ->
         for (i in 0 until width) {
             var previous = 10
             var next = 10
-            val current = getNumericValue(it[i])
+            val current = it[i]
             if (i - 1 >= 0) {
-                previous = getNumericValue(it[i - 1])
+                previous = it[i - 1]
             }
-
             if (i + 1 < width) {
-                next = getNumericValue(it[i + 1])
+                next = it[i + 1]
             }
-
             if (current < next && current < previous) {
-
-                var upper = 10
-                var lower = 10
+                var upper = 9
+                var lower = 9
 
                 if (index != 0) {
-                    upper = getNumericValue(input[index - 1][i])
+                    upper = grid[index - 1][i]
                 }
 
                 if (index != height - 1) {
-                    lower = getNumericValue(input[index + 1][i])
+                    lower = grid[index + 1][i]
                 }
 
                 if (current < upper && current < lower) {
-                    result += (current + 1)
+                    result.add(Pair(index, i))
                 }
-
             }
         }
     }
     return result
-}
-
-fun stageTwo(input: List<String>): Int {
-    TODO("Implement later")
-    return 0
 }
